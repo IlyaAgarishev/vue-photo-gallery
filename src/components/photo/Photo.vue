32888cff7de9f66ee162c8ba6e679db2fcc3e17d
IlyaAgarishev <ilyagarishev@gmail.com>
2022-07-05 07:31:31 +0500

    drew data

---
+++ src/components/photo/Photo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ photo.id + photo.title }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    photo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>PHOTOS</div>
+  <v-container>
+    <Photo v-for="photo in photos" :photo="photo" />
+  </v-container>
 </template>
 
 <script>
-export default {};
+import Photo from "../components/photo/Photo";
+
+export default {
+  components: { Photo },
+
+  data: () => ({
+    photos: [
+      { id: 1, title: "фото 1" },
+      { id: 2, title: "фото 2" },
+      { id: 3, title: "фото 3" },
+      { id: 4, title: "фото 4" },
+    ],
+  }),
+};
 </script>
 
 <style lang="scss" scoped></style>
