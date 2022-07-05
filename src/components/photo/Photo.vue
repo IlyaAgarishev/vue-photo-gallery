6cd299d1a6670102ec55a417896a4d8d1e2a20e8
IlyaAgarishev <ilyagarishev@gmail.com>
2022-07-06 00:05:54 +0500

    1)added FileReader 2) PhotoForm 3) func addPhoto

--- src/components/photo/Photo.vue
+++ src/components/photo/Photo.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>{{ photo.id + photo.title }}</div>
+  <v-col cols="4">
+    <v-card>
+      <v-card-title>
+        {{ photo.title }}
+      </v-card-title>
+      <v-card-text>
+        <v-img width="200" height="200" :src="photo.url"></v-img>
+      </v-card-text>
+    </v-card>
+  </v-col>
 </template>
 
 <script>
--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,22 +1,21 @@
 <template>
   <v-container>
-    <Photo v-for="photo in photos" :photo="photo" />
+    <PhotoForm @addPhoto="addPhoto" />
+    <v-row>
+      <Photo v-for="photo in photos" :photo="photo" />
+    </v-row>
   </v-container>
 </template>
 
 <script>
 import Photo from "../components/photo/Photo";
+import PhotoForm from "../components/photo/PhotoForm";
 
 export default {
-  components: { Photo },
+  components: { Photo, PhotoForm },
 
   data: () => ({
-    photos: [
-      { id: 1, title: "фото 1" },
-      { id: 2, title: "фото 2" },
-      { id: 3, title: "фото 3" },
-      { id: 4, title: "фото 4" },
-    ],
+    photos: [],
   }),
   mounted() {
     this.fetchTodo();
@@ -26,6 +25,10 @@ export default {
       this.axios
         .get("https://jsonplaceholder.typicode.com/photos?_limit=10")
         .then((resp) => (this.photos = resp.data));
+    },
+
+    addPhoto(photo) {
+      this.photos.push(photo);
     },
   },
 };
