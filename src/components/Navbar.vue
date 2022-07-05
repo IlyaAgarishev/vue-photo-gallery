d871beb454ad5a0ae403cda4f00ce38a68018b67
IlyaAgarishev <ilyagarishev@gmail.com>
2022-07-05 06:30:06 +0500

    routing made

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid>
+    <v-app-bar color="orange" dense dark>
+      <v-spacer />
+      <v-btn outlined class="ml-3">
+        <router-link to="/">Главная</router-link>
+      </v-btn>
+      <v-btn outlined class="ml-3">
+        <router-link to="/photo">Фотографии</router-link>
+      </v-btn>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/pages/PhotosPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>PHOTOS</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
